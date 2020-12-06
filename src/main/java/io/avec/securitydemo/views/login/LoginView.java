cd9b61eecf724cb0cadd49666145a2cf560eaba0
Avec <avec112@gmail.com>
2020-12-06 15:41:38 +0100

    - Upgrade Vaadin 14.4.4 - Upgrade Spring Boot 2.4.0 - Added login info on login page - Demo of hidden menu item based on privileges - vaadin.pnpm.enable=true

--- src/main/java/io/avec/securitydemo/views/login/LoginView.java
+++ src/main/java/io/avec/securitydemo/views/login/LoginView.java
@@ -1,6 +1,9 @@
 package io.avec.securitydemo.views.login;
 
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
@@ -23,10 +26,21 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
         login.setAction("login");
 
+        final Div usersDiv = new Div(new UnorderedList(
+                new ListItem("guest/guest - No privileges"),
+                new ListItem("user/user - Restricted privileges"),
+                new ListItem("admin/admin - All privileges")
+        ));
+        usersDiv.getStyle()
+                .set("background", "white")
+                .set("background", "var(--lumo-base-color) linear-gradient(var(--lumo-tint-20pct), var(--lumo-tint-20pct))")
+                .set("padding", "var(--lumo-space-l)");
         add(
                 new H1("Vaadin with Spring Security"),
+                usersDiv,
                 login
         );
+
     }
 
 
--- src/main/java/io/avec/securitydemo/views/main/MainView.java
+++ src/main/java/io/avec/securitydemo/views/main/MainView.java
@@ -1,5 +1,7 @@
 package io.avec.securitydemo.views.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import com.vaadin.flow.component.Component;
@@ -22,6 +24,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import io.avec.securitydemo.security.SecurityUtils;
 import io.avec.securitydemo.views.allroles.AllrolesView;
 import io.avec.securitydemo.views.contentdifferencesprrole.ContentdifferencesprRoleView;
 import io.avec.securitydemo.views.adminroleonly.AdminRoleonlyView;
@@ -97,11 +100,17 @@ public class MainView extends AppLayout {
     }
 
     private Component[] createMenuItems() {
-        return new Tab[] {
-            createTab("All", AllrolesView.class),
-            createTab("Restricted", ContentdifferencesprRoleView.class),
-            createTab("Admin", AdminRoleonlyView.class)
-        };
+        List<Tab> tabs = new ArrayList<>();
+        tabs.add(createTab("All", AllrolesView.class));
+        tabs.add(createTab("Restricted", ContentdifferencesprRoleView.class));
+        tabs.add(createTab("Admin (link visible for all)", AdminRoleonlyView.class)); // visible
+
+        // demo how to hide menu item
+        if(SecurityUtils.isAccessGranted(AdminRoleonlyView.class)) {
+            tabs.add(createTab("Admin (link visible only for admin)", AdminRoleonlyView.class)); // visible for admin only
+        }
+
+        return tabs.toArray(new Tab[0]);
     }
 
     private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
